98a59bca5f11c74a5b9b5f2925332ec2a5cce533
vialeon <dnewberry21@amherst.edu>
2021-04-24 16:59:35 -0400

    add all aforementioned to ext so the functions get registered

--- pkg/ext/ext.go
+++ pkg/ext/ext.go
@@ -1,8 +1,12 @@
 package ext
 
 import (
+	"strings"
+
+	"github.com/augmentable-dev/flite/internal/http_request"
 	"github.com/augmentable-dev/flite/internal/lines"
 	"github.com/augmentable-dev/flite/internal/readfile"
+	"github.com/augmentable-dev/flite/internal/str"
 	"github.com/augmentable-dev/flite/internal/yaml"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -10,6 +14,13 @@ import (
 )
 
 func init() {
+	split := func(s, c string, i int) string {
+		split := strings.Split(s, c)
+		if i < len(split) {
+			return split[i]
+		}
+		return ""
+	}
 	sqlite.Register(func(api *sqlite.ExtensionApi) (sqlite.ErrorCode, error) {
 		if err := api.CreateModule("lines", lines.NewVTab(),
 			sqlite.EponymousOnly(true), sqlite.ReadOnly(true)); err != nil {
@@ -27,10 +38,17 @@ func init() {
 		if err := api.CreateFunction("json_to_yaml", yaml.NewJSONToYAML()); err != nil {
 			return sqlite.SQLITE_ERROR, err
 		}
-		// if err := api.CreateFunction("http_request", http_request.NewHttp_request()); err != nil {
-		// 	return sqlite.SQLITE_ERROR, err
-		// }
+		if err := api.CreateFunction("http_get", http_request.New_get()); err != nil {
+			return sqlite.SQLITE_ERROR, err
+		}
+		if err := api.CreateFunction("http_post", http_request.New_post()); err != nil {
+			return sqlite.SQLITE_ERROR, err
+		}
+		if err := api.CreateFunction("str_split", str.Newstr_split); err != nil {
+			return sqlite.SQLITE_ERROR, err
+		}
 
 		return sqlite.SQLITE_OK, nil
 	})
 }
+func main() {}
